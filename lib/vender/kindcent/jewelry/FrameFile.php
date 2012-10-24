f41844708ac92af9245fc723be9ed17f74cb45f3
Ryan <cumt.xiaochi@gmail.com>
2012-10-24 22:31:19 +0800

    file load (customize for each project)

---
+++ lib/vender/kindcent/jewelry/FrameFile.php
@@ -0,0 +1,14 @@
+/**
+ * 现在看来这个也可以用来 js css 啥的
+ * Usage: FrameFile::controller('index');
+ */
+
+namespace kindcent\jewelry;
+
+class FrameFile
+{
+    public static function controller()
+    {
+        
+    }
+}
