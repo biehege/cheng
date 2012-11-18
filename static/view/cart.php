7d74fb609ec6160d4682d7863d4b2495bcfc40c7
Ryan <cumt.xiaochi@gmail.com>
2012-11-18 15:08:29 +0800

    cart css basicly ok

--- static/view/cart.php
+++ static/view/cart.php
@@ -5,20 +5,23 @@
  */
 ?>
 <div class="indicator">
-    <span>选购货品</span>
-    <span>核对货品及收件地址</span>
-    <span>提交订单</span>
+    <span>1、选购货品</span>
+    <span class="sep"></span>
+    <span class="on">2、核对货品及收件地址</span>
+    <span class="sep"></span>
+    <span>3、提交订单</span>
 </div>
-<div>
+<div class="title">
     <h2>我的购物车</h2>
-    <a href="<?= ROOT ?>">返回继续选购</a>
+    <a href="<?= ROOT ?>">&lt;&lt;返回继续选购</a>
 </div>
 <?php if ($orders_count): ?>
     <h3>确认订单信息</h3>
-    <div>
+    <div class="table-title-row">
         <span class="col name title">货品名称</span>
-        <span class="col info title">详细参数</span>
-        <span class="col price title">预计价格</span>
+        <span class="col e-info title">详细参数</span>
+        <span class="col num title">数量</span>
+        <span class="col price title">英格预估价</span>
         <span class="col del title">删除</span>
     </div>
     <?php foreach ($orders as $order): ?>
@@ -26,22 +29,30 @@
         <div class="product-entry" data-id="<?= $order->id ?>">
             <div class="row" >
                 <div class="col name">
-                    <img />
-                    <span><?= $prd->name ?></span>
-                    <span>货号：<?= $prd->no ?></span>
+                    <div class="image-wrap">
+                        <img src="<?= $prd->image1_thumb ?>" />
+                    </div>
+                    <div class="text-wrap">
+                        <span class="text"><?= $prd->name ?></span>
+                        <span>货号：<?= $prd->no ?></span>
+                    </div>
                 </div>
-                <div class="col info">
-                    <span>材质：<?= $order->material ?></span>
-                    <span>手寸：<?= $order->size ?></span>
-                    <span>刻字：<?= $order->carve_text ?></span>
-                    <span>镶口：<?= $prd->rabbet_start ?>-<?= $prd->rabbet_end ?> ct</span>
-                    <span>辅石：<?= $prd->small_stone ?>粒</span>
-                    <span>工费：<?= $labor_expense ?>元/件</span>
-                    <span>损耗：<?= $wear_tear ?>%</span>
+                <div class="col e-info">
+                    <div>
+                        <span class="">材质：<?= $order->material ?></span>
+                        <span class="e">手寸：<?= $order->size ?></span>
+                        <span class="e">刻字：<?= $order->carve_text ?></span>
+                        <span class="e">镶口：<?= $prd->rabbet_start ?>-<?= $prd->rabbet_end ?> ct</span>
+                    </div>
+                    <div>
+                        <span class="">辅石：<?= $prd->small_stone ?>粒</span>
+                        <span class="e">工费：<?= $labor_expense ?>元/件</span>
+                        <span class="e">损耗：<?= $wear_tear ?>%</span>
+                    </div>
                 </div>
+                <div class="col num">1</div>
                 <div class="col price">
-                    <div>英格预估价</div>
-                    <div><?= $order->estimate_price ?>元</div>
+                    <div>￥<?= $order->estimate_price ?></div>
                 </div>
                 <div class="col del">
                     <div class="popup">
@@ -52,24 +63,24 @@
                     <span class="del btn">删除</span>
                 </div>
             </div>
-            <div class="remark">
-                <form class="remark" method="post" action="<?= ROOT ?>order">
-                    <input type="hidden" name="target" value="<?= $order->id ?>" />
-                    <input type="hidden" name="action" value="change_remark">
-                    <input type="text" name="remark" placeholder="填写备注信息&gt;" value="<?= $order->customer_remark ?>" />
+            <form class="remark" method="post" action="<?= ROOT ?>order">
+                <input type="hidden" name="target" value="<?= $order->id ?>" />
+                <input type="hidden" name="action" value="change_remark">
+                <div class="remark">
+                    <input class="text-in" type="text" name="remark" placeholder="填写备注信息&gt;" value="<?= $order->customer_remark ?>" />
                     <input type="submit" value="确定">
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     <?php endforeach ?>
     <div class="total-info">
-        <span>
-            共计
+        <span class="count">
+            共计：
             <strong><?= $cart->count() ?>件</strong>
         </span>
-        <span>
-            预估总价
-            <strong><?= $cart->totalPrice() ?>元</strong>
+        <span class="price">
+            预估总价：
+            <strong>￥<?= $cart->totalPrice() ?></strong>
         </span>
     </div>
 <?php else: ?>
@@ -77,17 +88,17 @@
 <?php endif ?>
 
 <form method="post">
-    <div>
+    <div class="addr-info">
         <span>请确认您的收件地址：</span>
         <span class="edit-addr-btn">编辑</span>
     </div>
     <?php foreach ($addresses as $addr): ?>
-        <div>
+        <div class="addr">
             <input type="radio" name="address" value="<?= $addr->id ?>" id="addr<?= $addr->id ?>" data-id="<?= $addr->id ?>" checked />
             <label for="addr<?= $addr->id ?>"><?= $addr->name ?></label>
             <span><?= $addr->phone ?></span>
             <span><?= $addr->detail ?></span>
         </div>
     <?php endforeach ?>
-    <input type="submit" value="提交订单" />
+    <input type="submit" value="提交订单" class="submit-btn" />
 </form>
--- static/view/header.php
+++ static/view/header.php
@@ -2,7 +2,7 @@
 !defined('IN_PTF') && exit('ILLEGAL EXECUTION');
 ?>
 <div class="small-wrap">
-    <div class="price">
+    <div class="gold-price">
         <span class="lbl">今日金价</span>
         <span class="info">PT950: <?= Price::current('PT950') ?></span>
         <span class="info">AU750: <?= Price::current('AU750') ?></span>
@@ -12,17 +12,15 @@
             <?php if ($user_type === 'Customer'): ?>
                 <span class="welcom">你好，<?= htmlentities($user->name) ?></span>
                 <?php if ($user_type === 'Customer'): ?>
-
                     <a class="cart" href="<?= ROOT ?>cart">购物车 (<span class="count"><?= $cart->count() ?></span>)</a>
                 <?php endif; ?>
-                
                 <a href="<?= ROOT ?>order/all" class="my-order">我的订单</a>
             <?php else: ?>
                 <span><?= htmlentities($user->name) ?></span>
                 <a href="<?= ROOT ?>order/all">管理中心</a>
             <?php endif ?>
             
-            <a href="<?= ROOT ?>login?logout=1">logout</a>
+            <a href="<?= ROOT ?>login?logout=1">注销</a>
         <?php else: ?>
             <a href="<?= ROOT ?>login?back=<?= $request_uri ?>">登录</a>
             <a href="<?= ROOT ?>register?back=<?= $request_uri ?>">注册</a>
