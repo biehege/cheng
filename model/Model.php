a5ca9567ea492733d8cc8f0467a97b0b47b0127e
Ryan <cumt.xiaochi@gmail.com>
2012-11-23 15:42:36 +0800

    add create and update

--- model/Model.php
+++ model/Model.php
@@ -21,13 +21,39 @@ class Model
             throw new Exception("not good arg for construct");
         }
     }
-    
+
+    public static function create($info = array())
+    {
+        $class_vars = get_class_vars(get_class($this));
+        $table = $class_vars['table'];
+
+        Pdb::insert($info, $table);
+        return new self(Pdb::lastInsertId());
+    }
+
     public function selfCond() 
     {
         return array('id=?' => $this->id);
     }
 
     public function edit($key_or_array, $value = null)
+    {
+        if($value !== null) { // given by key => value
+            $arr = array($key_or_array => $value);
+        } else {
+            $arr = $key_or_array;
+        }
+
+        // why we need that, fuck!
+        $class_vars = get_class_vars(get_class($this));
+        $table = $class_vars['table'];
+
+        Pdb::update($arr, $table, $this->selfCond()); // why we need that? that doesn't make any sense
+        $this->info = $this->info(); // refresh data
+    }
+
+    // function same as edit()
+    public function update($key_or_array, $value = null)
     {
         if($value !== null) { // given by key => value
             $arr = array($key_or_array => $value);
